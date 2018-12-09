7e96cd3e98915f3f1702607bf1b30f7c7e9cf53e
Reed Martz <reed@reedmartz.com>
2018-12-09 10:45:23 -0500

    Fixup vestigial remnants from Day 5 code

--- 2018/Day 5/remove_blocking_element.py
+++ 2018/Day 5/remove_blocking_element.py
@@ -4,14 +4,13 @@ def remove_paired_letters(line):
         a, b = line[index:index + 2]
         if a != b and a.lower() == b.lower():
             line = line[:index] + line[index + 2:]
-            changed = True
             index = max(index - 1, 0)
         else:
             index += 1
     return line
 
 def find_shortest_processed_lines(line):
-    elements = '0abcdefghijklmnopqrstuvwxyz'
+    elements = 'abcdefghijklmnopqrstuvwxyz'
     for elem in elements:
         stripped_string = ''.join(l for l in line if l.lower() != elem)
         yield len(remove_paired_letters(stripped_string))
