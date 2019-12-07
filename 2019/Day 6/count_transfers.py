c8c196749aa050127a86bd7d7159fa97148031cb
Reed Martz <reed@reedmartz.com>
2019-12-06 21:28:55 -0500

    Add Day 6

---
+++ 2019/Day 6/count_transfers.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+from functools import lru_cache
+
+relationships = {}
+
+def list_ancestors(id):
+    while True:
+        try:
+            parent = relationships[id]
+        except KeyError:
+            return
+        yield parent
+        id = parent
+
+
+def count_transfers(src, dst):
+    src_ancestors = set(list_ancestors(src))
+    dst_ancestors = set(list_ancestors(dst))
+
+    common_ancestors = src_ancestors.intersection(dst_ancestors)
+
+    return len(src_ancestors) + len(dst_ancestors) - 2 * len(common_ancestors)
+
+
+with open('data.txt', 'r') as fp:
+    orbit_pairs = (line.strip().split(')') for line in fp)
+    for target, satelite in orbit_pairs:
+        relationships[satelite] = target
+
+    print(count_transfers('YOU', 'SAN'))
