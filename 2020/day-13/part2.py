aa9964700bbef118d457296e0e45315b61e6a498
Reed Martz <reed@reedmartz.com>
2020-12-13 08:57:47 -0500

    Use LCM, still non-performant

--- 2020/day-13/part2.py
+++ 2020/day-13/part2.py
@@ -1,4 +1,17 @@
 from functools import reduce
+from collections import namedtuple, Counter, defaultdict
+from itertools import chain
+
+BusSchedule = namedtuple("BusSchedule", ['offset', 'interval'])
+
+
+def prime_factors(val):
+    for p in chain([2], count(start=3, step=2)):
+        while val % p == 0:
+            yield p
+            val //= p
+        if val == 1:
+            return
 
 
 def count(start=0, step=1):
@@ -16,22 +29,66 @@ def product(it):
     return reduce(lambda val, acc: val * acc, it, 1)
 
 
-def buses_arrive_consecutive_minutes(t, bus_pairs):
-    for offset, interval in bus_ids:
-        if wait_next_arrival(t, interval) != offset:
-            return False
-    return True
+def lcm(nums):
+    nums = list(nums)
+
+    factors = defaultdict(int)
+    for num in nums:
+        counts = Counter(prime_factors(num))
+        for p, count in counts.items():
+            factors[p] = max(factors[p], count)
+
+    return product(p ** count for p, count in factors.items())
+
+
+def buses_arrive_on_consecutive_minutes(t, schedules):
+    for schedule in schedules:
+        wait_until = wait_next_arrival(t, schedule.interval)
+        if wait_until == schedule.offset:
+            yield schedule
+
+
+assert lcm([3, 9]) == 9
+assert lcm([3, 5]) == 15
+assert lcm([4, 78, 43, 13, 8, 25]) == 335400
 
 
 with open("./data.txt", "r") as fp:
     _, bus_ids = fp.readlines()
 
-    bus_ids = [(offset, int(interval)) for offset, interval in enumerate(bus_ids.split(',')) if interval != 'x']
+ids = bus_ids.split(',')
 
-    start, step = max(bus_ids, key=lambda bus_id: bus_id[1])
+schedules = set(BusSchedule(offset=offset, interval=int(interval)) for offset, interval in enumerate(ids) if interval != 'x')
 
-    for t in count(start=start, step=step):
-        if buses_arrive_consecutive_minutes(t, bus_ids):
-            print(t)
-            break
+slowest = min(schedules, key=lambda schedule: schedule.interval)
 
+# The solution requires all buses to be aligned with their offset
+# Find buses that are aligned, and increment such that they will remain aligned while
+#   we attempt to align with more buses.
+# Start with the slowest bus, and increment by that bus's interval
+# If a 60 minute bus with offset 10 works at 70 minutes, and also at 130 minutes
+# When we align with other buses, we step by the LCM of all found bus intervals
+# e.g., if a 60 minute and 90 minute bus are aligned, they will be aligned precisely once every 180 minutes
+t = slowest.offset
+step = slowest.interval
+fits = set([slowest])
+remainder = schedules - fits
+cycle_loop = lcm(schedule.interval for schedule in schedules)
+
+while remainder:
+    if t > cycle_loop * max(schedule.interval for schedule in remainder) * 1000:
+        raise Exception("No solution found")
+    t += step
+
+    new_fits = set(buses_arrive_on_consecutive_minutes(t, remainder))
+    if new_fits:
+        fits |= new_fits
+        remainder -= new_fits
+        print(fits)
+        print(remainder)
+        step = lcm(schedule.interval for schedule in fits)
+        t %= step
+        print(step)
+
+
+print(t % step)
