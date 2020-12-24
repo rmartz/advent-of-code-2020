934e540c90326e5fa9c033287b631e79c996d5d2
Reed Martz <reed@reedmartz.com>
2020-12-24 18:39:19 -0500

    Clean up

--- 2020/day-19/part1.py
+++ 2020/day-19/part1.py
@@ -5,6 +5,7 @@ import re
 
 rules = {}
 
+
 @lru_cache
 def generate_regex(id):
     global rules
@@ -13,36 +14,33 @@ def generate_regex(id):
         return rule
 
     # Otherwise the rule is a list of possible rules that we want to evaluate and combine
-    parts = [''.join(generate_regex(sub_id) for sub_id in branch) for branch in rule]
+    parts = ["".join(generate_regex(sub_id) for sub_id in branch) for branch in rule]
 
     return f"({'|'.join(parts)})"
 
 
 def load_rule(line):
-    id, rule = line.split(': ')
+    id, rule = line.split(": ")
     id = int(id)
     if '"' in rule:
         return (id, rule[1])
 
-    branches = rule.split(' | ')
-    sub_ids = [[int(id) for id in branch.split(' ')] for branch in branches]
+    branches = rule.split(" | ")
+    sub_ids = [[int(id) for id in branch.split(" ")] for branch in branches]
     return (id, sub_ids)
 
 
 def load_rules(lines):
     rules = (load_rule(line) for line in lines)
-    return {
-        id: rule
-        for id, rule in rules
-    }
+    return {id: rule for id, rule in rules}
 
 
 def check_input(regex, line):
     return regex.match(line)
 
 
-def find_matching_lines(rules, rule_id, lines):
-    rule_regex = re.compile(f'^{generate_regex(rule_id)}$')
+def find_matching_lines(rule_id, lines):
+    rule_regex = re.compile(f"^{generate_regex(rule_id)}$")
     for line in lines:
         if rule_regex.match(line):
             yield line
@@ -60,5 +58,5 @@ with open("./data.txt", "r") as fp:
 
     # Second segment is strings to match
     input_lines = next(segments)
-    matching_lines = find_matching_lines(rules, 0, input_lines)
+    matching_lines = find_matching_lines(0, input_lines)
     print(sum(1 for line in matching_lines))
